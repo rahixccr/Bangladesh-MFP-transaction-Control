f7af27d1cc57d7157e4f44deee642fbc8d594218
Muntashir Are rahi <33172858+rahixccr@users.noreply.github.com>
2021-01-01 17:40:32 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 	$host = "localhost";
-	$user = "sparkitb_sadmin";
-	$password = "spark1tl9941992";
-	$db = "sparkitb_smsdb";
+	$user = "root";
+	$password = "";
+	$db = "mfs";
 	
 	$connect = mysqli_connect($host,$user,$password,$db) or die(mysqli_error($connect));
 	
